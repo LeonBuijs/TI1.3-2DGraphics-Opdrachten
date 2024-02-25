be5f8e45f7fe894405aac7cc7ba855fc558d63f6
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-02-25 14:18:50 +0100

    Animation werkt
    Opdracht 3-2 werkt

--- Week3/002.MovingCharacter/src/MovingCharacter.java
+++ Week3/002.MovingCharacter/src/MovingCharacter.java
@@ -20,24 +20,40 @@ import org.jfree.fx.ResizableCanvas;
 
 public class MovingCharacter extends Application {
     private ResizableCanvas canvas;
+    private BufferedImage[] tiles;
+    private double current = 32;
+    private double x = 0;
+    private boolean mousePressed = false;
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
+        try {
+            BufferedImage image = ImageIO.read(getClass().getResource("/images/sprite.png"));
+            tiles = new BufferedImage[65];
+
+            for (int i = 0; i < 65; i++)
+                tiles[i] = image.getSubimage(64 * (i % 8), 64 * (i / 8), 64, 64);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        canvas.setOnMousePressed(event -> {
+            this.current = 64;
+            this.mousePressed = true;
+            this.current = 40;
+        });
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
         new AnimationTimer() {
             long last = -1;
 
             @Override
-            public void handle(long now)
-            {
+            public void handle(long now) {
                 if (last == -1)
                     last = now;
-                update((now - last) / 1000000000.0);
+                update((now - last) / 1.0e8);
                 last = now;
                 draw(g2d);
             }
@@ -50,20 +66,42 @@ public class MovingCharacter extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+    public void draw(FXGraphics2D graphics) {
+        AffineTransform affineTransform = new AffineTransform();
+        graphics.setTransform(affineTransform);
+        graphics.setBackground(Color.black);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        graphics.drawImage(tiles[(int) Math.floor(this.current)], (int) this.x, 0, null);
     }
 
 
-    public void update(double deltaTime)
-    {
+    public void update(double deltaTime) {
+        this.x += deltaTime * 2;
+        if (this.x >= canvas.getWidth()) {
+            this.x = -64;
+        }
+
+        if (this.mousePressed){
+            if ((this.current) <= 47) {
+                this.current += deltaTime;
+            } else {
+                this.current = 64;
+                this.mousePressed = false;
+            }
+        } else {
+            if ((this.current) <= 39) {
+                this.current += deltaTime;
+            } else {
+                this.current = 32;
+            }
+        }
+    }
+    public void mousePressed(){
+        this.current = 64;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(MovingCharacter.class);
     }
 
