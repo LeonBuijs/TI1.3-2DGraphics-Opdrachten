b4a6f0b582809a786ceebd4f7882a2276f302a0f
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-02-26 09:08:23 +0100

    Opdracht imagesfading werkt

--- Week3/003.FadingImage/src/FadingImage.java
+++ Week3/003.FadingImage/src/FadingImage.java
@@ -15,9 +15,18 @@ import org.jfree.fx.ResizableCanvas;
 
 public class FadingImage extends Application {
     private ResizableCanvas canvas;
-    
+    private float imgOpacity = 0.0f;
+    private int img1 = 0;
+    private int img2 = 1;
+    private Image[] images;
+
     @Override
     public void start(Stage stage) throws Exception {
+        images = new Image[]{ImageIO.read(getClass().getResource("image1.jpg")),
+                ImageIO.read(getClass().getResource("image2.jpg")),
+                ImageIO.read(getClass().getResource("image3.jpg")),
+                ImageIO.read(getClass().getResource("image4.jpg")),
+                ImageIO.read(getClass().getResource("image5.jpg"))};
 
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
@@ -29,7 +38,7 @@ public class FadingImage extends Application {
             public void handle(long now) {
 		if(last == -1)
                     last = now;
-		update((now - last) / 1000000000.0);
+		update((now - last) / 0.5e10);
 		last = now;
 		draw(g2d);
             }
@@ -46,11 +55,27 @@ public class FadingImage extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int)canvas.getWidth(), (int)canvas.getHeight());
+
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1 - this.imgOpacity));
+        graphics.drawImage(images[img1], 0,0, null);
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, this.imgOpacity));
+        graphics.drawImage(images[img2], 0,0, null);
     }
     
 
     public void update(double deltaTime) {
-	
+        if (this.imgOpacity + deltaTime > 1.0f){
+            this.imgOpacity = 0;
+            img1++;
+            img2++;
+            if (img1 >= 5){
+                img1 = 0;
+            }
+            if (img2 >= 5){
+                img2 = 0;
+            }
+        }
+	    this.imgOpacity += (float) deltaTime;
     }
 
     public static void main(String[] args) {
